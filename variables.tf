4ade242814f44270be228d2832b8833933c78dda
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-07-24 10:13:10 +0200

    adding deployment-tags

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "location" {
   default = "France Central"
 }
 variable "name-spokes" {
-  description = "provide cidr of spokes"
+  description = "provide cidr of spokes, e.g."
   default = {
     "0" = ["spoke-A","frontend","backend","application"]
     "1" = ["spoke-B","frontend","backend","application"]   
@@ -11,10 +11,17 @@ variable "name-spokes" {
   } 
 }
 variable "name-spokes" {
-  description = "provide names of spokes"
+  description = "provide names of spokes, e.g."
   default = {
     "0" = ["10.0.0.0/22","10.0.0.0/24","10.0.1.0/24","10.0.2.0/24"]
     "1" = ["10.0.4.0/22","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
   # "2" = ["10.0.8.0/22","10.0.8.0/24","10.0.9.0/24","10.0.10.0/24"]
+  }
+}
+variable "deployment-tags" {
+  description = "provide tag values, e.g."
+  default = {
+    owner       = "administrator"
+    managed-by  = "terraform-cloud"
   }
 }
